d93a2742b0122bee357da905f1d4d9a131515651
Tiago Magalhães <tmagdev@gmail.com>
2024-05-12 23:03:13 +0100

    Adding basic shading using unit vector normals.

--- Source/Maths/RayUtilities.h
+++ Source/Maths/RayUtilities.h
@@ -1,23 +1,36 @@
 #ifndef RAYTRACER_RAYUTILITIES_H
 #define RAYTRACER_RAYUTILITIES_H
 
+#include <cmath>
+
 #include "Vector3.h"
 #include "Ray.h"
 
 namespace RayTracer {
 
-    inline bool HitSphere(const Point3& center, float radius, const Ray& ray) {
+    inline float HitSphere(const Point3& center, float radius, const Ray& ray) {
         Vector3 origin = center - ray.Origin();
         float a = RayTracer::Dot(ray.Direction(), ray.Direction());
         float b = -2.0f * RayTracer::Dot(ray.Direction(), origin);
         float c = RayTracer::Dot(origin, origin) - radius * radius;
         float discriminant = b * b - 4 * a * c;
-        return discriminant >= 0;
+
+        if (discriminant < 0) {
+            return -1;
+        }
+
+        return (-b - std::sqrt(discriminant)) / (2 * a);
     }
 
     inline Color GetRayColor(const Ray& ray) {
-        if (RayTracer::HitSphere(Point3{0, 0, -1}, 0.5, ray)) {
-            return Color{1, 0, 0};
+        auto raycast = RayTracer::HitSphere(Point3{0, 0, -1}, 0.5, ray);
+        if (raycast > 0) {
+            Vector3 raycastUnitDirection = RayTracer::UnitVector(ray.At(raycast) - Vector3{0, 0, -1});
+            return 0.5 * Color{
+                    raycastUnitDirection.X() + 1,
+                    raycastUnitDirection.Y() + 1,
+                    raycastUnitDirection.Z() + 1
+            };
         }
 
         Vector3 unitDirection = RayTracer::UnitVector(ray.Direction());
