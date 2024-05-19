23c1385d4f87ec8cde108000f243f0d527f7df1e
Tiago Magalhães <tmagdev@gmail.com>
2024-05-19 16:02:29 +0100

    Created a world to render one final scene.

--- Source/Engine.cpp
+++ Source/Engine.cpp
@@ -1,5 +1,4 @@
 #include "Engine.h"
-#include "Camera/Camera.h"
 #include "Renderers/FileRenderer.h"
 #include "Objects/Sphere.h"
 #include "Materials/Lambertian.h"
@@ -8,25 +7,78 @@
 
 namespace RayTracer {
     void Engine::Trace() {
-        // Build the world.
+        const HittableVector& world = CreateWorld();
+        Camera camera = CreateCamera();
+
+        camera.Render(world);
+    }
+
+    HittableVector Engine::CreateWorld() {
         HittableVector world;
 
-        float radius = std::cos(Constants::Pi / 4);
+        auto groundMaterial = std::make_shared<Lambertian>(Color{0.5f, 0.5f, 0.5f});
+        world.Add(std::make_shared<Sphere>(Point3{0, -1000.0f, 0}, 1000, groundMaterial));
 
-        auto materialLeft = std::make_shared<Lambertian>(Color{0, 0, 1});
-        auto materialRight = std::make_shared<Lambertian>(Color{1, 0, 0});
+        auto offsetPoint = Point3{4, 0.2f, 0};
+        for (int a = -11; a < 11; a++) {
+            for (int b = -11; b < 11; b++) {
+                auto aAsFloat = static_cast<float>(a);
+                auto bAsFloat = static_cast<float>(b);
+                Point3 center{aAsFloat + 0.9f * Utilities::RandomFloat(), 0.2f,
+                              bAsFloat + 0.9f * Utilities::RandomFloat()};
 
-        world.Add(std::make_shared<Sphere>(Point3{-radius, 0, -1.0f}, radius, materialLeft));
-        world.Add(std::make_shared<Sphere>(Point3{radius, 0, -1.0f}, radius, materialRight));
+                if ((center - offsetPoint).Length() > 0.9) {
+                    float chooseMaterial = Utilities::RandomFloat();
+                    std::shared_ptr<Sphere> sphere;
 
+                    if (chooseMaterial < 0.8) {
+                        // Diffuse.
+                        Color albedo = Random() * Random();
+                        auto sphereMaterial = std::make_shared<Lambertian>(albedo);
+                        sphere = std::make_shared<Sphere>(center, 0.2f, sphereMaterial);
+                    } else if (chooseMaterial < 0.95) {
+                        // Metal.
+                        Color albedo = Random(0.5, 1);
+                        auto fuzz = Utilities::RandomFloat(0, 0.5);
+                        auto sphereMaterial = std::make_shared<Metal>(albedo, fuzz);
+                        sphere = std::make_shared<Sphere>(center, 0.2f, sphereMaterial);
+                    } else {
+                        // Glass.
+                        auto sphereMaterial = std::make_shared<Dielectric>(0.2);
+                        sphere = std::make_shared<Sphere>(center, 0.2f, sphereMaterial);
+                    }
+
+                    world.Add(sphere);
+                }
+            }
+        }
+
+        auto largeSphere1Material = std::make_shared<Dielectric>(1.5);
+        world.Add(std::make_shared<Sphere>(Point3{0, 0, 1}, 1.0, largeSphere1Material));
+
+        auto largeSphere2Material = std::make_shared<Lambertian>(Color{0.4f, 0.2f, 0.1f});
+        world.Add(std::make_shared<Sphere>(Point3{-4, 1, 0}, 1.0, largeSphere2Material));
+
+        auto largeSphere3Material = std::make_shared<Metal>(Color{0.7f, 0.6f, 0.5f}, 0.0f);
+        world.Add(std::make_shared<Sphere>(Point3{4, 1, 0}, 1.0, largeSphere3Material));
+
+        return world;
+    }
+
+    Camera Engine::CreateCamera() {
         Camera camera{std::make_unique<FileRenderer>()};
 
         camera.AspectRatio = 16.0f / 9.0f;
-        camera.ImageWidth = 640; // This produces a 640x360 image.
-        camera.SamplesPerPixel = 100;
+        camera.ImageWidth = 1920; // This produces a 1920x1080 image.
+        camera.SamplesPerPixel = 500;
         camera.MaxDepth = 50;
-        camera.VerticalFOV = 90;
+        camera.VerticalFOV = 20;
+        camera.LookFrom = Point3{13, 2, 3};
+        camera.LookAt = Point3{0, 0, 0};
+        camera.Up = Vector3{0, 1, 0};
+        camera.DefocusAngle = 0.6f;
+        camera.FocusDistance = 10.0f;
 
-        camera.Render(world);
+        return camera;
     }
 }
--- Source/Engine.h
+++ Source/Engine.h
@@ -1,11 +1,17 @@
 #ifndef RAYTRACER_ENGINE_H
 #define RAYTRACER_ENGINE_H
 
+#include "Camera/Camera.h"
+
 namespace RayTracer {
 
     class Engine {
     public:
         static void Trace();
+
+    private:
+        static HittableVector CreateWorld();
+        static Camera CreateCamera();
     };
 
 }
