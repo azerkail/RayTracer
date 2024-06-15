9457a59ffbf6fcf9e8a99994e435b0009842432b
Tiago Magalhães <tmagdev@gmail.com>
2024-06-15 13:31:49 +0100

    Adjusting the phase for a more marble like texture.

--- Source/Textures/NoiseTexture.cpp
+++ Source/Textures/NoiseTexture.cpp
@@ -8,6 +8,6 @@ namespace RayTracer
 
     Color NoiseTexture::Value([[maybe_unused]] float u, [[maybe_unused]] float v, const Point3& result) const
     {
-        return Color{1, 1, 1} * m_noise.Turbulance(result, 7);
+        return Color{.5, .5, .5} * (1 + std::sin(m_scale * result.Z() + 10 * m_noise.Turbulance(result, 7)));
     }
 }
